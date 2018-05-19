02bbf56bc47f3b054f53896a72830f4b3e95c05c
erick <erick@hotmail.ca>
2018-05-19 15:11:43 -0400

    Add character with basic movement

---
+++ ConcordiaGamejam/Assets/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float maxSpeed = 10f;
+    public float jumpForce = 300f;
+
+    private Rigidbody2D rb2d;
+    public Transform GroundCheck;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+    private float groundRadius = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+        if (isGrounded && Input.GetKeyDown(KeyCode.X)) {
+            rb2d.AddForce(new Vector2(0, jumpForce));
+        }
+	}
+
+	void FixedUpdate () {
+
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, groundRadius, groundLayer);
+
+        // side to side movement
+        float moveH = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(moveH*maxSpeed, rb2d.velocity.y);
+
+        Debug.Log("Grounded: " + isGrounded);
+	}
+}
