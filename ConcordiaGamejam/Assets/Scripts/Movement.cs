57656ec822489096607caf2f7736ba7ccd772f40
erick <erick@hotmail.ca>
2018-05-19 19:50:42 -0400

    Add basic animations for soldier

--- ConcordiaGamejam/Assets/Scripts/Movement.cs
+++ ConcordiaGamejam/Assets/Scripts/Movement.cs
@@ -2,49 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
     public float maxSpeed = 10f;
     public float jumpForce = 300f;
     public static bool isFacingRight = true;
-    public static Vector2 direction = new Vector2(0,0);
+    public static Vector2 direction = new Vector2(0, 0);
 
     private Rigidbody2D rb2d;
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
     public Transform GroundCheck;
     public LayerMask groundLayer;
     private bool isGrounded;
     private float groundRadius = 0.2f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-	}
+        animator = GetComponent<Animator>();
+    }
 
-	void Update()
-	{
-        if (isGrounded && Input.GetKeyDown(KeyCode.X)) {
+    void Update()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.X))
+        {
             rb2d.AddForce(new Vector2(0, jumpForce));
         }
 
-        spriteRenderer.color = isFacingRight ? Color.cyan : Color.blue;
+        spriteRenderer.flipX = !isFacingRight;
+        animator.SetBool("isFacingRight", isFacingRight);
+        animator.SetBool("isGrounded", isGrounded);
 
-	}
+    }
 
-	void FixedUpdate () {
+    void FixedUpdate()
+    {
 
         isGrounded = Physics2D.OverlapCircle(GroundCheck.position, groundRadius, groundLayer);
 
         // side to side movement
         float moveH = Input.GetAxis("Horizontal");
-        rb2d.velocity = new Vector2(moveH*maxSpeed, rb2d.velocity.y);
+        rb2d.velocity = new Vector2(moveH * maxSpeed, rb2d.velocity.y);
 
-        if (isFacingRight && moveH < 0) {
+        if (isFacingRight && moveH < 0)
+        {
             isFacingRight = false;
-        } else if (!isFacingRight && moveH > 0) {
+
+        }
+        else if (!isFacingRight && moveH > 0)
+        {
             isFacingRight = true;
         }
 
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-	}
+    }
 }
