826ea5165f4e2e9c0a68910b5c05eb2a864d157b
erick <erick@hotmail.ca>
2018-05-20 13:48:36 -0400

    Add menu

---
+++ ConcordiaGamejam/Assets/Scripts/GoToMainScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GoToMainScene : MonoBehaviour
+{
+
+    private Text t;
+    private Button button;
+    private GameObject parentCanvas;
+
+    // Use this for initialization
+    void Start()
+    {
+        parentCanvas = GameObject.FindGameObjectWithTag("menuCanvas");
+        t = GetComponentInChildren<Text>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+}
